4c77dbf0b963cc20f6f10cbbd0a92b1e01b69d7e
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2024-12-05 16:20:11 +0900

    feat: server action

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,21 +3,28 @@ import FormInput from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
 
 export default function LogIn() {
+  async function handleForm(formData: FormData) {
+    "use server";
+    await new Promise(resolve => setTimeout(resolve, 5000));
+    console.log(formData.get("email"), formData.get("password"));
+    console.log("logged in!")
+  }
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">Log in with email and password.</h2>
       </div>
-      <form className="flex flex-col gap-3">
-        <FormInput type="email" placeholder="Email" required errors={[]} />
+      <form action={handleForm} className="flex flex-col gap-3">
+        <FormInput name="email" type="email" placeholder="Email" required errors={[]} />
         <FormInput
+          name="password"
           type="password"
           placeholder="Password"
           required
           errors={[]}
         />
-        <FormButton loading={false} text="Log in" />
+        <FormButton text="Log in" />
       </form>
       <SocialLogin />
     </div>
